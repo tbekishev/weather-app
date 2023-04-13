72d50942a55a7347b43da8e03fbc918e8cc142c2
Tuigun Bekishev <tbekishev@gmail.com>
2023-04-13 14:26:01 -0300

    fetch data, add dynamic states

--- src/App.js
+++ src/App.js
@@ -1,37 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import snowBg from "./assets/snow.jpg";
 import hotBg from "./assets/sun.jpg";
 import Details from "./components/Details";
+import { getWeatherData } from "./weatherApi";
 
 function App() {
+
+  const [weather, setWeather] = useState(null);
+  const [units, setUnits] = useState('metric');
+
+  useEffect(() => {
+
+    const weatherData = async() => {
+      const data = await getWeatherData('75009', units);
+      setWeather(data);
+    };
+
+    weatherData();
+  }, []);
 
   return (
     <div className="App" style={{ backgroundImage: `url(${hotBg})`}}>
       <div className="overlay">
+        { weather && (
         <div className="container">
           <div className="section section__inputs">
             <input 
               type="text" 
               name="city"
-              placeholder="Enter City..."
+              placeholder="Enter zip code..."
             />
             <button>Search</button>
           </div>
           <div className="section section__temperature">
             <div className="icon">
-              <h3>Tampa, FL</h3>
+              <h3>{`${weather.name}, ${weather.country}`}</h3>
               <img 
                 alt="weatherIcon" 
-                src="https://openweathermap.org/img/wn/02d@2x.png"
+                src={weather.iconUrl}
               />
-              <h3>Sunny</h3>
+              <h3>{weather.description}</h3>
             </div>
             <div className="temperature">
-              <h1>100 F</h1>
+              <h1>{weather.temp.toFixed()} °{units === 'metric' ? 'C': 'F'}</h1>
             </div>
           </div>
-          <Details />
+          <Details 
+            weather={weather}
+            units={units}
+          />
         </div>
+        )}
       </div>
     </div>
   );
--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,38 +1,73 @@
 import React from 'react';
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
+import CompressIcon from '@mui/icons-material/Compress';
+import WaterDropIcon from '@mui/icons-material/WaterDrop';
+import AirIcon from '@mui/icons-material/Air';
 import './Details.scss';
 
-const Details = () => {
+const Details = ({ weather, units }) => {
+
+  const tempUnit = units === 'metric' ? '°C': '°F';
+  const windUnit = units === 'metric' ? 'm/s': 'm/h';
+
+  const cards = [
+    {
+      id: 1,
+      icon: <ArrowDownwardIcon />,
+      title: 'min',
+      data: weather.temp_min.toFixed(),
+      unit: tempUnit
+    },
+    {
+      id: 2,
+      icon: <ArrowUpwardIcon />,
+      title: 'max',
+      data: weather.temp_max.toFixed(),
+      unit: tempUnit
+    },
+    {
+      id: 3,
+      icon: <InsertEmoticonIcon />,
+      title: 'feels like',
+      data: weather.feels_like.toFixed(),
+      unit: tempUnit
+    },
+    {
+      id: 4,
+      icon: <CompressIcon />,
+      title: 'pressure',
+      data: weather.pressure,
+      unit: 'hPa'
+    },
+    {
+      id: 5,
+      icon: <WaterDropIcon />,
+      title: 'humidity',
+      data: weather.humidity,
+      unit: '%'
+    },
+    {
+      id: 6,
+      icon: <AirIcon />,
+      title: 'wind speed',
+      data: weather.speed.toFixed(),
+      unit: windUnit
+    }
+  ]
+
   return (
     <div className='section section__details'>
-      <div className='card'>
-        <div className='details__card-icon'>
-          <ArrowDownwardIcon />
-          <small>min</small>
-        </div>
-        <h2>32 F</h2>
-      </div>
-      <div className='card'>
-        <div className='details__card-icon'>
-          <ArrowDownwardIcon />
-          <small>min</small>
-        </div>
-        <h2>32 F</h2>
-      </div>
-      <div className='card'>
-        <div className='details__card-icon'>
-          <ArrowDownwardIcon />
-          <small>min</small>
-        </div>
-        <h2>32 F</h2>
-      </div>
-      <div className='card'>
-        <div className='details__card-icon'>
-          <ArrowDownwardIcon />
-          <small>min</small>
-        </div>
-        <h2>32 F</h2>
-      </div>
+      {cards.map(({ id, icon, title, data, unit}) => (
+        <div key={id} className='card'>
+          <div className='details__card-icon'>
+            {icon}
+            <small>{title}</small>
+          </div>
+          <h2>{`${data} ${unit}`}</h2>
+        </div>        
+      ))}
     </div>
   )
 };
